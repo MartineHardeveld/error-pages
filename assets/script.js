89c1bc506571c13c111f6d728a917bf0d000bab9
mtimofiiv <github@fiiv.io>
2016-11-02 12:01:37 +0100

    update with translation

---
+++ assets/script.js
@@ -0,0 +1,36 @@
+/*
+  This key is a public access key, no need to hide it
+  https://github.com/Giphy/GiphyAPI
+*/
+var GIPHY_KEY = 'dc6zaTOxFJmzC';
+
+function insetBackground(payload) {
+  if (!payload.data || !Array.isArray(payload.data)) return;
+
+  var randomNumber = Math.floor(Math.random() * payload.data.length);
+  var chosenImage = payload.data[randomNumber];
+  var gif = chosenImage.images.original.url;
+  var body = document.getElementsByTagName('body')[0];
+
+  body.style.backgroundImage = 'url(' + gif + ')';
+}
+
+function init(apiKey) {
+  var xhr = new XMLHttpRequest();
+  var uri = '//api.giphy.com/v1/gifs/search';
+
+  uri += '?api_key=' + apiKey;
+  uri += '&q=' + 'fail';
+  uri += '&rating=' + 'pg13';
+  uri += '&offset=' + Math.floor(Math.random() * 100);
+
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState !== XMLHttpRequest.DONE || xhr.status !== 200) return;
+    insetBackground(JSON.parse(xhr.response));
+  }
+
+  xhr.open('GET', uri, true);
+  xhr.send();
+}
+
+init(GIPHY_KEY);
