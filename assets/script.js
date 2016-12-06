d16d36c6272749b2c910cfa752cdf1c645aebf4e
Mike Timofiiv <github@fiiv.io>
2016-12-06 10:48:05 +0100

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -15,6 +15,16 @@ function insetBackground(payload) {
   body.style.backgroundImage = 'url(' + gif + ')';
 }
 
+function tracking() {
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-88574480-1', 'auto');
+  ga('send', 'pageview');
+}
+
 function init(apiKey) {
   var xhr = new XMLHttpRequest();
   var uri = '//api.giphy.com/v1/gifs/search';
@@ -34,3 +44,4 @@ function init(apiKey) {
 }
 
 init(GIPHY_KEY);
+tracking();
